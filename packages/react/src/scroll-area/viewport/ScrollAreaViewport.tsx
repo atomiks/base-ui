eae56dcbe824ecddd0a3b75bf4e80abbb8eadcd9
atomiks <cc.glows@gmail.com>
2025-09-18 19:37:37 +1000

    lint

--- packages/react/src/scroll-area/viewport/ScrollAreaViewport.tsx
+++ packages/react/src/scroll-area/viewport/ScrollAreaViewport.tsx
@@ -1,6 +1,5 @@
 'use client';
 import * as React from 'react';
-import * as ReactDOM from 'react-dom';
 import { useEventCallback } from '@base-ui-components/utils/useEventCallback';
 import { useIsoLayoutEffect } from '@base-ui-components/utils/useIsoLayoutEffect';
 import { useTimeout } from '@base-ui-components/utils/useTimeout';
@@ -55,7 +54,7 @@ export const ScrollAreaViewport = React.forwardRef(function ScrollAreaViewport(
   const programmaticScrollRef = React.useRef(true);
   const scrollEndTimeout = useTimeout();
 
-  function computeThumbPositionHandler() {
+  const computeThumbPosition = useEventCallback(() => {
     const viewportEl = viewportRef.current;
     const scrollbarYEl = scrollbarYRef.current;
     const scrollbarXEl = scrollbarXRef.current;
@@ -226,10 +225,6 @@ export const ScrollAreaViewport = React.forwardRef(function ScrollAreaViewport(
       }
       return nextOverflowEdges;
     });
-  }
-
-  const computeThumbPosition = useEventCallback(() => {
-    ReactDOM.flushSync(computeThumbPositionHandler);
   });
 
   useIsoLayoutEffect(() => {
